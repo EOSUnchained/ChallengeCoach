1204114745a0261dc562476a9c35e08c6ef1f4f2
mohammed tahir <tahir_mohammed@rocketmail.com>
2018-11-11 11:10:10 +0000

    added a staking mechanism

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -18,48 +18,6 @@ const endpoint = "http://localhost:8888";
 
 // NEVER store private keys in any source code in your real life development
 // This is for demo purposes only!
-const accounts = [
-  {
-    name: "useraaaaaaaa",
-    privateKey: "5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5",
-    publicKey: "EOS6kYgMTCh1iqpq9XGNQbEi8Q6k5GujefN9DSs55dcjVyFAq7B6b"
-  },
-  {
-    name: "useraaaaaaab",
-    privateKey: "5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg",
-    publicKey: "EOS78RuuHNgtmDv9jwAzhxZ9LmC6F295snyQ9eUDQ5YtVHJ1udE6p"
-  },
-  {
-    name: "useraaaaaaac",
-    privateKey: "5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7",
-    publicKey: "EOS5yd9aufDv7MqMquGcQdD6Bfmv6umqSuh9ru3kheDBqbi6vtJ58"
-  },
-  {
-    name: "useraaaaaaad",
-    privateKey: "5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx",
-    publicKey: "EOS8LoJJUU3dhiFyJ5HmsMiAuNLGc6HMkxF4Etx6pxLRG7FU89x6X"
-  },
-  {
-    name: "useraaaaaaae",
-    privateKey: "5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg",
-    publicKey: "EOS7XPiPuL3jbgpfS3FFmjtXK62Th9n2WZdvJb6XLygAghfx1W7Nb"
-  },
-  {
-    name: "useraaaaaaaf",
-    privateKey: "5KaqYiQzKsXXXxVvrG8Q3ECZdQAj2hNcvCgGEubRvvq7CU3LySK",
-    publicKey: "EOS5btzHW33f9zbhkwjJTYsoyRzXUNstx1Da9X2nTzk8BQztxoP3H"
-  },
-  {
-    name: "useraaaaaaag",
-    privateKey: "5KFyaxQW8L6uXFB6wSgC44EsAbzC7ideyhhQ68tiYfdKQp69xKo",
-    publicKey: "EOS8Du668rSVDE3KkmhwKkmAyxdBd73B51FKE7SjkKe5YERBULMrw"
-  },
-  {
-    name: "eosunchained",
-    privateKey: "5KHNxA3DVa3WJJqNNeJKiteyZPoZAuoGkKtwYyZLGxq5aXmPhdG",
-    publicKey: "EOS7yAUWVL7HPMr6SQr8ojT5otg1xbnGytBgGGYVEc3Qks2uJwavZ"
-  }
-];
 // set up styling classes using material-ui "withStyles"
 const styles = theme => ({
   card: {
@@ -86,9 +44,14 @@ class Index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      noteTable: [] // to store the table rows from smart contract
+      noteTable: [], // to store the table rows from smart contract
+      price1: "20",
+      price2: "50",
+      price3: "100",
+      selectedPrice: "0"
     };
     this.handleFormEvent = this.handleFormEvent.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   // generic function to handle form events (e.g. "submit" / "reset")
@@ -100,7 +63,7 @@ class Index extends Component {
     // collect form data
     let account = event.target.account.value;
     let privateKey = event.target.privateKey.value;
-    let note = event.target.note.value;
+    let stakeAmount = event.target.stakeAmount.value;
 
     // prepare variables for the switch below to send transactions
     let actionName = "";
@@ -112,7 +75,7 @@ class Index extends Component {
         actionName = "update";
         actionData = {
           user: account,
-          note: note
+          stakeAmount: stakeAmount
         };
         break;
       default:
@@ -161,6 +124,9 @@ class Index extends Component {
     }
   }
 
+  handleClick(price) {
+    this.setState({ selectedPrice: price });
+  }
   // gets table data from the blockchain
   // and saves it into the component state: "noteTable"
   getTable() {
@@ -185,7 +151,7 @@ class Index extends Component {
     const { classes } = this.props;
 
     // generate each note as a card
-    const generateCard = (key, timestamp, user, note) => (
+    const generateCard = (key, timestamp, user, stakeAmount) => (
       <Card className={classes.card} key={key}>
         <CardContent>
           <Typography variant="headline" component="h2">
@@ -198,12 +164,12 @@ class Index extends Component {
           >
             {new Date(timestamp * 1000).toString()}
           </Typography>
-          <Typography component="pre">{note}</Typography>
+          <Typography component="pre">{stakeAmount}</Typography>
         </CardContent>
       </Card>
     );
     let noteCards = noteTable.map((row, i) =>
-      generateCard(i, row.timestamp, row.user, row.note)
+      generateCard(i, row.timestamp, row.user, row.stakeAmount)
     );
 
     return (
@@ -232,15 +198,30 @@ class Index extends Component {
               margin="normal"
               fullWidth
             />
-            <TextField
-              name="note"
-              autoComplete="off"
-              label="Note (Optional)"
-              margin="normal"
-              multiline
-              rows="10"
-              fullWidth
+            <input
+              type="radio"
+              name="stakeAmount"
+              value={this.state.price1}
+              checked={this.state.price1 === this.state.selectedPrice}
+              onChange={() => this.handleClick(this.state.price1)}
             />
+            {this.state.price1}
+            <input
+              type="radio"
+              name="stakeAmount"
+              value={this.state.price2}
+              checked={this.state.price2 === this.state.selectedPrice}
+              onChange={() => this.handleClick(this.state.price2)}
+            />
+            {this.state.price2}
+            <input
+              type="radio"
+              name="stakeAmount"
+              value={this.state.price3}
+              checked={this.state.price3 === this.state.selectedPrice}
+              onChange={() => this.handleClick(this.state.price3)}
+            />
+            {this.state.price3}
             <Button
               variant="contained"
               color="primary"
@@ -251,13 +232,6 @@ class Index extends Component {
             </Button>
           </form>
         </Paper>
-        <pre className={classes.pre}>
-          Below is a list of pre-created accounts information for add/update
-          note:
-          <br />
-          <br />
-          accounts = {JSON.stringify(accounts, null, 2)}
-        </pre>
       </div>
     );
   }
